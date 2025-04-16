7b5c1e691e18370265122c842b94eff3fcd5b4e2
LikeAaron <aaron.vanmarcke@student.howest.be>
2025-04-16 16:00:41 +0200

    removed unneeded code

--- Client/gui/main_window.py
+++ Client/gui/main_window.py
@@ -5,30 +5,30 @@ from Client.gui.login import LoginFrame
 from Client.gui.register import RegisterFrame
 import Client.gui.frames as frames
 from Client.gui.frames import AppController
-class MainWindow:
-    def __init__(self, root):
-        self.root = root
-        self.root.title("Arrest App")
-        self.root.geometry("300x300")
+# class MainWindow:
+#     def __init__(self, root):
+#         self.root = root
+#         self.root.title("Arrest App")
+#         self.root.geometry("300x300")
 
-        # Set the actual class references
-        frames.LoginFrame = LoginFrame
-        frames.RegisterFrame = RegisterFrame
+#         # Set the actual class references
+#         frames.LoginFrame = LoginFrame
+#         frames.RegisterFrame = RegisterFrame
 
-        self.container = tk.Frame(self.root)
-        self.container.pack(fill="both", expand=True)
+#         self.container = tk.Frame(self.root)
+#         self.container.pack(fill="both", expand=True)
 
-        self.frames = {}
-        for F in (LoginFrame, RegisterFrame):
-            frame = F(self.container, self)
-            self.frames[F] = frame
-            frame.grid(row=0, column=0, sticky="nsew")
+#         self.frames = {}
+#         for F in (LoginFrame, RegisterFrame):
+#             frame = F(self.container, self)
+#             self.frames[F] = frame
+#             frame.grid(row=0, column=0, sticky="nsew")
 
-        self.show_frame(LoginFrame)
+#         self.show_frame(LoginFrame)
 
-    def show_frame(self, frame_class):
-        frame = self.frames[frame_class]
-        frame.tkraise()
+#     def show_frame(self, frame_class):
+#         frame = self.frames[frame_class]
+#         frame.tkraise()
 
 def start_gui():
     root = tk.Tk()
